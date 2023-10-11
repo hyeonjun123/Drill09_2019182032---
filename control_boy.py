189f59a777364cb72e76f973bab9d342c7c8930a
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-10-11 19:46:59 +0900

    Drill09 시작

--- control_boy.py
+++ control_boy.py
@@ -1,41 +1,13 @@
 from pico2d import *
-import random
 
+from grass import Grass
+from boy import Boy
 
 # Game object class here
-
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-
-    def update(self):
-        pass
-
-
-class Boy:
-    def __init__(self):
-        self.x, self.y = 400, 90
-        self.frame = 0
-        self.dir = 0
-        self.action = 3
-        self.image = load_image('animation_sheet.png')
-
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-
-    def handle_event(self, event):
-        pass
-
-    def draw(self):
-        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
 
 
 def handle_events():
     global running
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -43,7 +15,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         else:
-            boy.handle_event(event)
+            boy.handle_event(event) #여기 이벤트는 키다운 디스케이프같은 거이다.
 
 
 def reset_world():
@@ -84,6 +56,6 @@ while running:
     handle_events()
     update_world()
     render_world()
-    delay(0.05)
+    delay(0.01)
 # finalization code
 close_canvas()
